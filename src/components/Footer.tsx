fcaf53ddd1780165b07a31f46e427848d5f6ab5c
Adrian Boros <adiboros@hotmail.com>
2025-09-27 13:50:00 +0300

    fix: some cleanup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,4 @@
 import { h } from "preact";
-import { useAppStore } from "@state/AppStore";
-import { useEffect, useRef, useState } from "preact/hooks";
 import { route } from "preact-router";
 
 type FooterProps = {
@@ -17,16 +15,6 @@ export function Footer({
   optionBtn = true,
 }: FooterProps) {
 
-  // useEffect(() => {
-  //   const handleKeyDown = (e: KeyboardEvent) => {
-  //     if (e.key === "SoftRight") {
-  //       route("/settings");
-  //       e.preventDefault();
-  //     }
-  //   };
-  //   window.addEventListener("keydown", handleKeyDown);
-  //   return () => window.removeEventListener("keydown", handleKeyDown);
-  // }, [route]);
   return (
     <footer className="flex bg-white-90 items-center justify-between w-full fixed b text-3xl">
       {backBtn && <div id="softkey-left" className="px-4">Back</div>}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,8 @@
 import { h } from "preact";
-//import { Header } from "@components/Header";
-// import { getLocation } from "@lib/hashHistory";
-// import Splash from "@pages/Splash";
+
 export default function Layout({ children }: { children?: preact.ComponentChildren }) {
-
-  // const location = getLocation();
-  // console.log("Layout", location);
-  // const isSplash = location.pathname === "/" || location.pathname === "/setup";
-
-  // console.log("isSplash", isSplash);
   return (
     <div className="min-h-screen w-full">
-      {/* <Header /> */}
       <main className="mx-auto">{children}</main>
 
     </div>
